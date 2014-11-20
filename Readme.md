2b66d235aafd350fec2fda0a7e5a02c68225a1f4
Daniel Fagnan <dnfagnan@gmail.com>
2014-11-20 09:42:02 -0700

    init

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# 3-Axis Gimbal Software
+
+The software that powers our 3-axis gimbal is all written in [Rust](http://rust-lang.org).
+
+## Getting Started
+
+You need the following requirements:
+
+* Install the nightly build of Rust (This software will be keeping up with the nightlies as best as it can)
+
+
+## License
+
+MIT
---
+++ src/lib.rs
@@ -0,0 +1,22 @@
+#![no_std]
+#![feature(lang_items, asm, phase)]
+
+#[lang = "stack_exhausted"] extern fn stack_exhausted() {}
+#[lang = "eh_personality"] extern fn eh_personality() {}
+#[lang = "panic_fmt"] fn panic_fmt() -> ! { loop {} }
+#[cfg(not(test))]
+#[lang="begin_unwind"]
+extern fn begin_unwind() {}
+
+#[lang="sized"]
+pub trait Sized for Sized? {}
+
+#[no_mangle]
+pub unsafe extern fn main() {
+    run();
+    loop {};    
+}
+
+fn run() {
+    unsafe { asm!("nop") }
+}
